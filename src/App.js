7685c2694bedd2a4f6c47c1a2252a83f18c50f2a
Patrick Oser <patrick.oser148@gmail.com>
2023-11-30 15:42:15 -0500

    Import everything needed to App.js.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import Header from './header';
+import Nav from './nav';
+import Footer from './footer';
+import Home from './home';
+import NewPost from './newPost';
+import PostPage from './postPage';
+import About from './about';
+import Missing from './missing';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import { useState, useEffect } from './react';
 
 function App() {
   return (
     <div className="App">
-      
+
     </div>
   );
 }
