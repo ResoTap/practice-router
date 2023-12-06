b9c6d24b357710cba4e7bb4ff82411806705ebf1
Patrick Oser <patrick.oser148@gmail.com>
2023-12-06 14:56:04 -0500

    Set form for new post submittion.

--- src/newPost.js
+++ src/newPost.js
@@ -1,7 +1,29 @@
-const NewPost = () => {
+const NewPost = ({
+    handleSubmit, postTitle, setPostTitle, postBody, setPostBody
+}) => {
     return (
-        <main>
-            <h1>New Post</h1>
+        <main className="NewPost">
+            <h2>New Post</h2>
+            <form className="newPostForm" onSubmit={handleSubmit}>
+                <label htmlFor="postTitle">Title:</label>
+                <input 
+                    type="text" 
+                    id="postTitle"
+                    required
+                    value={postTitle}
+                    onChange={(e) => setPostTitle=(e.target.value)}
+                />
+                <label htmlFor="postBody">Post:</label>
+                <textarea 
+                    name="postBody" 
+                    id="postBody"
+                    required
+                    value={postBody}
+                    onChange={(e) => setPostBody(e.target.value)} 
+                    cols="30" 
+                    rows="10"
+                ></textarea>
+            </form>
         </main>
     )
 }
