f9463bc26f9fafa2b555fdcbc73831e82614f112
Patrick Oser <patrick.oser148@gmail.com>
2023-12-06 12:31:08 -0500

    Import Post to feed file.

--- src/feed.js
+++ src/feed.js
@@ -1,3 +1,5 @@
+import Post from "./post"
+
 const Feed = ({ posts }) => {
     return (
         <>
--- src/post.js
+++ src/post.js
@@ -1,4 +1,4 @@
-const Posts = () => {
+const Post = () => {
     return (
         <div>
 
@@ -6,4 +6,4 @@ const Posts = () => {
     )
 }
 
-export default Posts
+export default Post
