729f558abe215e3a64ca20bebb6beff30c557ad1
Patrick Oser <patrick.oser148@gmail.com>
2023-12-16 17:03:15 -0500

    Set react icons in Header to display device specific icons depending on screen width.

--- src/header.js
+++ src/header.js
@@ -4,6 +4,9 @@ const Header = ({ title, width }) => {
     return (
         <header className="Header">
             <h1>{title}</h1>
+            {width < 768 ? <FaMobileAlt />
+                : width < 992 ? <FaTabletAlt />
+                : <FaLaptop />}
         </header>
     )
 }
