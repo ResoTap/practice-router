20f48c56aeccd437dc337e972267f69468847685
Patrick Oser <patrick.oser148@gmail.com>
2023-12-17 17:48:27 -0500

    Move imports to dataContext.js from App.js.

--- src/context/dataContext.js
+++ src/context/dataContext.js
@@ -1,4 +1,9 @@
 import { createContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { format } from 'date-fns';
+import api from '../api/posts';
+import useWindowSize from '../hooks/useWindowSize';
+import useAxiosFetch from '../hooks/useAxiosFetch';
 
 const DataContext = createContext({});
 
