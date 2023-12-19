47e642e2ddb6cf8ad33cdbcba7c70cefb2598c7f
Patrick Oser <patrick.oser148@gmail.com>
2023-12-19 16:14:46 -0500

    Add useEffect to Nav to update search results.

--- src/nav.js
+++ src/nav.js
@@ -8,6 +8,14 @@ const Nav = () => {
     const setSearch = useStoreActions((actions) => actions.setSearch);
     const setSearchResults = useStoreActions((actions) => actions.setSearchResults);
 
+    useEffect(() => {
+        const filteredResults = posts.filter(post => 
+            ((post.body).toLowerCase()).includes(search.toLowerCase())
+            || ((post.title).toLowerCase()).includes(search.toLowerCase()));
+
+        setSearchResults(filteredResults.reverse());
+    },[posts, search, setSearchResults]);
+
     return (
         <nav className="Nav">
             <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
