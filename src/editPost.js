f4900e13e5011a5b7d6a69ff3573d86b5af1afdf
Patrick Oser <patrick.oser148@gmail.com>
2023-12-19 16:45:33 -0500

    Update EditPost to be compatible with easy-peasy.

--- src/editPost.js
+++ src/editPost.js
@@ -1,16 +1,21 @@
-import { useEffect, useContext, useState } from "react";
+import { useEffect } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import { format } from "date-fns";
-import api from './api/posts'
-import DataContext from './context/dataContext';
+import { useStoreState, useStoreActions } from "easy-peasy"
 
 const EditPost = () => {
-    const { posts, setPosts } = useContext(DataContext);
-    const [editTitle, setEditTitle] = useState('');
-    const [editBody, setEditBody] = useState('');
     const { id } = useParams();
     const history = useNavigate();
-    const post = posts.find(post => (post.id).toString() === id);
+
+    const editTitle = useStoreState((state) => state.editTitle)
+    const editBody = useStoreState((state) => state.editBody);
+
+    const editPost = useStoreActions((actions) => actions.editPost);
+    const setEditTitle = useStoreActions((actions) => actions.setEditTitle);
+    const setEditBody = useStoreActions((actions) => actions.setEditBody);
+
+    const getPostById = useStoreState((state) => state.getPostById);
+    const post = getPostById(id);
 
     useEffect(() => {
         if (post) {
@@ -22,16 +27,8 @@ const EditPost = () => {
     const handleEdit = async (id) => {
         const datetime = format(new Date(), 'MMMM dd, yyyy pp');
         const updatedPost = { id, title: editTitle, datetime, body: editBody };
-        try {
-            const response = await api.put(`/posts/${id}`, updatedPost);
-            setPosts(posts.map(post => post.id === id ? { ...response.data } : post));
-            setEditTitle('');
-            setEditBody('');
-            history('/');
-        } catch (err) {
-            console.log(`Error: ${err.message}`);
-            console.log(`id: ${id}`);
-        }
+        editPost(updatedPost);
+        history(`/post/${id}`);
     }
 
     return (
