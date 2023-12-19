98b0b579b1a105af88a8b0929839d691425e7eaa
Patrick Oser <patrick.oser148@gmail.com>
2023-12-19 12:38:17 -0500

    Make Header and Footer siblings of DataProvider in jsx of App.

--- src/App.js
+++ src/App.js
@@ -14,19 +14,19 @@ function App() {
 
   return (
     <div className="App">
-      <DataProvider>
         <Header title="React JS Blog" />
-        <Nav />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/post' element={<NewPost />} />
-          <Route path='/edit/:id' element={<EditPost />} />
-          <Route path='/post/:id' element={<PostPage />} />
-          <Route path='/about' element={<About />} />
-          <Route path='*' element={<Missing />} />
-        </Routes>
+        <DataProvider>
+          <Nav />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/post' element={<NewPost />} />
+            <Route path='/edit/:id' element={<EditPost />} />
+            <Route path='/post/:id' element={<PostPage />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<Missing />} />
+          </Routes>
+        </DataProvider>
         <Footer />
-      </DataProvider>
     </div>
   );
 }
