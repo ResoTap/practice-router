fb4d4ddec0381acff8d04be1e5b49833f1ad50b7
Patrick Oser <patrick.oser148@gmail.com>
2023-12-20 14:50:08 -0500

    Remove async from handle functions.

--- src/editPost.js
+++ src/editPost.js
@@ -24,7 +24,7 @@ const EditPost = () => {
         }
     }, [post, setEditTitle, setEditBody])
 
-    const handleEdit = async (id) => {
+    const handleEdit = (id) => {
         const datetime = format(new Date(), 'MMMM dd, yyyy pp');
         const updatedPost = { id, title: editTitle, datetime, body: editBody };
         editPost(updatedPost);
